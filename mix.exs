eb4404d8c8ad1c6d233c25347e4752cd91cbae14
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2022-07-30 17:33:23 -0700

    Updating with package metadata

--- mix.exs
+++ mix.exs
@@ -7,6 +7,11 @@ defmodule Estate.MixProject do
       version: "1.0.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      description: "A macro that gives ecto schema modules behavior relating to state machines",
+      package: %{
+        links: %{"GitHub" => "https://github.com/krainboltgreene/estate.ex"},
+        licenses: ["Hippocratic-3.0"]
+      },
       deps: deps()
     ]
   end
